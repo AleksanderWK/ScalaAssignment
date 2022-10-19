68f9ab6b0d996923d007fdb29f1acb9ea0847c35
AleksanderWK <aleksawk@stud.ntnu.no>
2022-10-19 10:37:33 +0200

    Add tasks template

--- delivery_1/src/main/scala/Main.scala
+++ delivery_1/src/main/scala/Main.scala
@@ -1,3 +1,52 @@
+class Task1 {
+
+  def A(): Unit = {
+    print("Task 1A: \n")
+  }
+
+  def B(): Unit = {
+    print("Task 1B: \n")
+  }
+
+  def C(): Unit = {
+    print("Task 1C: \n")
+  }
+
+  def D(): Unit = {
+    print("Task 1D: \n")
+  }
+}
+
+class Task2() {
+
+  def A(): Unit = {
+    print("Task 2A: \n")
+  }
+
+  def B(): Unit = {
+    print("Task 2B: \n")
+  }
+
+  def C(): Unit = {
+    print("Task 2C: \n")
+  }
+
+  def D(): Unit = {
+    print("Task 2D: \n")
+  }
+}
+
+
 object Main extends App {
-  println("Hello, World!")
+  val task1 = new Task1
+  val task2 = new Task2
+  task1.A()
+  task1.B()
+  task1.C()
+  task1.D()
+  task2.A()
+  task2.B()
+  task2.C()
+  task2.D()
+
 }
