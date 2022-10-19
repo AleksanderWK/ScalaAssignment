b0cb28020d2693913345e25467f2c14d4a5f7612
Runar Sæther <runarsather@hotmail.com>
2022-10-19 14:08:58 +0200

    Use BigInt and reformat

--- delivery_1/src/main/scala/Main.scala
+++ delivery_1/src/main/scala/Main.scala
@@ -1,51 +1,58 @@
 class Task1 {
 
   def A(): Unit = {
-    println("Task 1A")
+    println("Task 1A:")
+
     var nums: List[Int] = List()
+
     for (n <- 1 to 50){
       nums = nums :+ n
     }
+
     println(nums)
-  }
-
-  def sum(list: List[Int]): Int = {
-    var sum: Int = 0
-
-    for (item <- list) {
-      sum += item
-    }
-
-    sum
   }
   
   def B(): Unit = {
     println("Task 1B:")
+
+    def sum(list: List[Int]): Int = {
+      var sum: Int = 0
+
+      for (item <- list) {
+        sum += item
+      }
+
+      sum
+    }
+
     println(sum(List(1, 2, 3)))
   }
 
-  def recursiveSum(list: List[Int], accSum: Int): Int = list match {
-    case Nil => accSum
-    case head :: tail => recursiveSum(tail, accSum + head)
-  } 
-
   def C(): Unit = {
     println("Task 1C:")
-    val test: List[Int] = List(1, 2, 3)
-    println(recursiveSum(test, 0))
-  }
 
-  def recursiveFib(n: Int, prev: Int = 1, prevprev: Int = 0): Int = {
-    n match {
-      case 0 => 0
-      case 1 => prev
-      case _ => recursiveFib(n-1, prev + prevprev, prev)
-    }
-  } 
+    def recursiveSum(list: List[Int], accSum: Int = 0): Int = {
+      list match {
+        case Nil => accSum
+        case head :: tail => recursiveSum(tail, accSum + head)
+      }
+    } 
+
+    println(recursiveSum(List(1, 2, 3)))
+  }
 
   def D(): Unit = {
     println("Task 1D:")
-    println(recursiveFib(5))
+
+    def recursiveFib(n: Int, prev: BigInt = 1, prevPrev: BigInt = 0): BigInt = {
+      n match {
+        case 0 => 0
+        case 1 => prev
+        case _ => recursiveFib(n-1, prev + prevPrev, prev)
+      }
+    } 
+
+    println(recursiveFib(10))
   }
 }
 
